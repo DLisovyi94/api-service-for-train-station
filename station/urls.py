43aca51043d8f5ce92d7d597b66e00eec9f629e7
Dmytro Lisovyi <kenedy11@ukr.net>
2025-06-27 15:42:31 +0300

    feat: register Trains in urls.py

--- station/urls.py
+++ station/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 from rest_framework import routers
 
 from station.views import (StationViewSet,
-                           RouteViewSet, JourneyViewSet)
+                           RouteViewSet, JourneyViewSet, TrainViewSet)
 
 router = routers.DefaultRouter()
 router.register("stations", StationViewSet)
 router.register("routes", RouteViewSet)
 router.register("journeys", JourneyViewSet)
+router.register("trains", TrainViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 

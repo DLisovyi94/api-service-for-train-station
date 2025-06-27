4490f4b2a268211b8b329027e775b36c8bb7350c
Dmytro Lisovyi <kenedy11@ukr.net>
2025-06-27 16:31:57 +0300

    feat: add ViewSet for model Order

--- station/views.py
+++ station/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import GenericViewSet
 
-from station.models import Station, Route, Journey, Train
+from station.models import Station, Route, Journey, Train, Order
 from station.serializers import (StationSerializer,
                                  StationListSerializer,
                                  StationDetailSerializer, RouteSerializer, RouteDetailSerializer, RouteListSerializer,
                                  JourneySerializer, JourneyListSerializer, JourneyDetailSerializer, TrainSerializer,
-                                 TrainListSerializer, TrainDetailSerializer)
+                                 TrainListSerializer, TrainDetailSerializer, OrderSerializer, OrderListSerializer)
 
 
 from django.db.models import Count
@@ -74,6 +76,30 @@ class TrainViewSet(viewsets.ModelViewSet):
         return TrainSerializer
 
 
+class OrderPagination(PageNumberPagination):
+    page_size = 5
+    max_page_size = 100
 
+
+class OrderViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet,
+):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    pagination_class = OrderPagination
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderListSerializer
+
+        return OrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 # Create your views here.
